e32e8efa11a5b5ec851680c43fe73ace17a03407
fajjarnr <zfajart@gmail.com>
2021-08-04 11:17:45 +0700

    route handler

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const coursesRouter = require("./routes/courses");
-const mediaRouter = require("./routes/media");
-const ordersRouter = require("./routes/orders");
-const paymentsRouter = require("./routes/payments");
 const usersRouter = require("./routes/users");
+const coursesRouter = require("./routes/courses");
+const chaptersRouter = require("./routes/chapters");
+const lessonsRouter = require("./routes/lessons");
+const mediaRouter = require("./routes/media");
+const orderPaymentsRouter = require("./routes/orderPayments");
 const refreshTokensRouter = require("./routes/refreshTokens");
+const mentorsRouter = require("./routes/mentors");
+const imageCoursesRouter = require("./routes/imageCourses");
+const myCoursesRouter = require("./routes/myCourses");
+const reviewsRouter = require("./routes/reviews");
+const webhookRouter = require("./routes/webhook");
 
 const verifyToken = require("./middlewares/verifyToken");
+const can = require("./middlewares/permission");
 
 const app = express();
 
@@ -23,11 +30,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/courses", verifyToken, coursesRouter);
-app.use("/media", mediaRouter);
-app.use("/orders", ordersRouter);
-app.use("/payments", paymentsRouter);
 app.use("/users", usersRouter);
+app.use("/courses", coursesRouter);
+app.use("/chapters", verifyToken, can("admin"), chaptersRouter);
+app.use("/lessons", verifyToken, can("admin"), lessonsRouter);
+app.use("/media", verifyToken, can("admin", "student"), mediaRouter);
+app.use("/orders", verifyToken, can("admin", "student"), orderPaymentsRouter);
 app.use("/refresh-tokens", refreshTokensRouter);
+app.use("/mentors", verifyToken, can("admin"), mentorsRouter);
+app.use("/image-courses", verifyToken, can("admin"), imageCoursesRouter);
+app.use("/my-courses", verifyToken, can("admin", "student"), myCoursesRouter);
+app.use("/reviews", verifyToken, can("admin", "student"), reviewsRouter);
+app.use("/webhook", webhookRouter);
 
 module.exports = app;
